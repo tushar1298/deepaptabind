420615266e1c95badb8279bf258eee495cee682d
Tushar Gupta <131578295+tushar1298@users.noreply.github.com>
2026-03-10 14:17:03 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -10,3 +10,15 @@ def safe_divide(a,b):
     if b==0:
         return 0
     return a/b
+import requests
+
+def fetch_pdb(pdb_id):
+
+    url = f"https://files.rcsb.org/download/{pdb_id}.pdb"
+
+    r = requests.get(url)
+
+    if r.status_code != 200:
+        return None
+
+    return r.text
