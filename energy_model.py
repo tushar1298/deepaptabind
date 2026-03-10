c6a91d76ec242a86e2422c1dc461d29ebc272e44
Tushar Gupta <131578295+tushar1298@users.noreply.github.com>
2026-03-10 14:31:16 +0530

    Update energy_model.py

--- energy_model.py
+++ energy_model.py
@@ -1,22 +1,15 @@
-from config import ENERGY_WEIGHTS
-
 def compute_binding_energy(features,interactions):
 
     contacts=features["contacts"]
     hbonds=interactions["hbonds"]
+    stacking=interactions["stacking"]
+    electro=interactions["electrostatic"]
 
     vdw = contacts * -0.10
-    electro = contacts * -0.05
-    hbond = hbonds * -0.50
-    stacking = contacts * -0.03
-    desolv = contacts * -0.02
+    hbond = hbonds * -1.5
+    stack = stacking * -0.7
+    elec = electro * -0.3
 
-    energy = (
-        ENERGY_WEIGHTS["vdw"]*vdw+
-        ENERGY_WEIGHTS["electro"]*electro+
-        ENERGY_WEIGHTS["hbond"]*hbond+
-        ENERGY_WEIGHTS["stacking"]*stacking+
-        ENERGY_WEIGHTS["desolv"]*desolv
-    )
+    energy = vdw + hbond + stack + elec
 
     return energy
