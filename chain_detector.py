0382a058fbad36eaa2bbc8cb48e3a64cb2389efe
Tushar Gupta <131578295+tushar1298@users.noreply.github.com>
2026-03-10 14:30:45 +0530

    Update chain_detector.py

--- chain_detector.py
+++ chain_detector.py
@@ -1,20 +1,20 @@
-from config import RNA_RES, PROTEIN_RES
+from config import RNA_RES, DNA_RES, PROTEIN_RES
 
 def detect_chains(structure):
 
     protein=set()
-    rna=set()
+    aptamer=set()
 
     for model in structure:
         for chain in model:
             for res in chain:
 
-                r=res.get_resname().strip()
+                name=res.get_resname().strip()
 
-                if r in RNA_RES:
-                    rna.add(chain.id)
-
-                if r in PROTEIN_RES:
+                if name in PROTEIN_RES:
                     protein.add(chain.id)
 
-    return list(protein), list(rna)
+                if name in RNA_RES or name in DNA_RES:
+                    aptamer.add(chain.id)
+
+    return list(protein), list(aptamer)
