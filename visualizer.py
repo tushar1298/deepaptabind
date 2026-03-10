acbdf9821216a2fd7dc386f4921df68faa0052ad
Tushar Gupta <131578295+tushar1298@users.noreply.github.com>
2026-03-10 14:32:51 +0530

    Update visualizer.py

--- visualizer.py
+++ visualizer.py
@@ -1,34 +1,20 @@
 import py3Dmol
 import streamlit as st
 
-def show_structure(pdb_string, protein_chain=None, rna_chain=None):
+def show_structure(pdb_string,protein_chain,aptamer_chain):
 
-    view = py3Dmol.view(width=900, height=600)
+    view=py3Dmol.view(width=900,height=600)
 
-    view.addModel(pdb_string, "pdb")
+    view.addModel(pdb_string,"pdb")
 
-    # Protein style
-    if protein_chain:
-        view.setStyle(
-            {"chain": protein_chain},
-            {"cartoon": {"color": "blue"}}
-        )
+    view.setStyle({"chain":protein_chain},
+                  {"cartoon":{"color":"blue"}})
 
-    # Aptamer style
-    if rna_chain:
-        view.setStyle(
-            {"chain": rna_chain},
-            {"stick": {"colorscheme": "greenCarbon"}}
-        )
-
-    # Default style
-    view.setStyle(
-        {"not": {"chain": [protein_chain, rna_chain]}},
-        {"cartoon": {"color": "grey"}}
-    )
+    view.setStyle({"chain":aptamer_chain},
+                  {"stick":{"colorscheme":"greenCarbon"}})
 
     view.zoomTo()
 
-    html = view._make_html()
+    html=view._make_html()
 
-    st.components.v1.html(html, height=600)
+    st.components.v1.html(html,height=600)
